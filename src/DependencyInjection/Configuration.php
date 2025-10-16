symfony/monolog-bundle

Add enabled parameter in configuration

DependencyInjection/Configuration.php
@@ -446,6 +446,7 @@ class Configuration implements ConfigurationInterface
                     ->end()
                 ->end()
                 ->scalarNode('id')->end() // service & rollbar
+                ->scalarNode('enabled')->defaultTrue()->end()
                 ->scalarNode('priority')->defaultValue(0)->end()
                 ->scalarNode('level')->defaultValue('DEBUG')->end()
                 ->booleanNode('bubble')->defaultTrue()->end()

sarim/symfony-monolog-bundle

Co-authored-by: Nicolas Grekas <nicolas.grekas@gmail.com>
Co-authored-by: Jérôme Tamarelle <jerome@tamarelle.net>

src/DependencyInjection/Configuration.php
@@ -457,6 +457,7 @@ class Configuration implements ConfigurationInterface
                     ->end()
                 ->end()
                 ->scalarNode('id')->end() // service & rollbar
+                ->booleanNode('enabled')->defaultTrue()->end()
                 ->scalarNode('priority')->defaultValue(0)->end()
                 ->scalarNode('level')->defaultValue('DEBUG')->end()
                 ->booleanNode('bubble')->defaultTrue()->end()